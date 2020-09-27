ea78ae2e3751b30f83126bf2022f4a225c5c355f
Charles Fiers <charlesfiers@gmail.com>
2020-09-27 18:06:47 +0200

    Création des premiers tableaux de mots

--- ionic_app/src/app/services/words/words.service.ts
+++ ionic_app/src/app/services/words/words.service.ts
@@ -5,5 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class WordsService {
 
+	private names = {
+		"objects" : [ "arbre", "panneaux", "voiture", "vélo", "buisson", "sapin", "piéton", "scooter", "route", "rivière"
+		],
+		"abstracts" : [ "rayon", "bruit", "odeur", "éclair", "rire", "cri", "lumière", "effluve"
+		],
+	};
+
+	private verbs = {
+		"actions" : [ "marche", "roule", "parle", "vend", "fond", "découvre", "coure"
+		],
+		"feelings" : [ "sen", "entend", "aperçoi", "touche", "goûte", "ressen"
+		],
+
+	};
+
   constructor() { }
 }
