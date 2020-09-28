6457c9e68ec47e55163292254d29ccdd76fe8e2d
Charles Fiers <charlesfiers@gmail.com>
2020-09-28 11:15:40 +0200

    Test Ionic pas concluant, création app Angular pour combinatoire

--- ionic_app/src/app/services/words/words.service.ts
+++ ionic_app/src/app/services/words/words.service.ts
@@ -1,24 +1,32 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
 export class WordsService {
 
-	private names = {
-		"objects" : [ "arbre", "panneaux", "voiture", "vélo", "buisson", "sapin", "piéton", "scooter", "route", "rivière"
-		],
-		"abstracts" : [ "rayon", "bruit", "odeur", "éclair", "rire", "cri", "lumière", "effluve"
-		],
+	private rand : number;
+
+	names = {
+		"objects" : [ "arbre", "panneaux", "voiture", "vélo", "buisson", "sapin", "piéton", "scooter", "route", "rivière"],
+		"abstracts" : [ "rayon", "bruit", "odeur", "éclair", "rire", "cri", "lumière", "effluve"]
 	};
 
-	private verbs = {
-		"actions" : [ "marche", "roule", "parle", "vend", "fond", "découvre", "coure"
-		],
-		"feelings" : [ "sen", "entend", "aperçoi", "touche", "goûte", "ressen"
-		],
-
+	verbs = {
+		"actions" : [ "marche", "roule", "parle", "vend", "fond", "découvre", "coure"],
+		"feelings" : [ "sen", "entend", "aperçoi", "touche", "goûte", "ressen"]
 	};
 
   constructor() { }
+
+  pickWord(type : string){
+
+  	switch (type) {
+  		case "objects":
+  			this.rand = Math.floor(Math.random() * this.names.objects.length);
+  			console.log(this.rand);
+  			return this.names.objects[0];
+  			break;
+  		
+  		default:
+  			return "ERROR";
+  			break;
+  	};
+  };
+
 }
