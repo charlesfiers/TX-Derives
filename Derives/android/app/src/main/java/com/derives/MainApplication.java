f133d29ba30533edf148ecc48d592347e57254b1
Charles Fiers <charlesfiers@gmail.com>
2020-12-06 16:32:50 +0100

    Ajout librairie Activity Recognition

--- Derives/android/app/src/main/java/com/derives/MainApplication.java
+++ Derives/android/app/src/main/java/com/derives/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.xebia.activityrecognition.RNActivityRecognitionPackage;
+import com.xebia.activityrecognition.RNActivityRecognitionPackage;
 import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.sensors.RNSensorsPackage;
